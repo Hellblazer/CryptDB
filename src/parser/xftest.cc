dbebda0abff90ffba7cb1fdf5fc6ea88a6db2b70
Nickolai Zeldovich <nickolai@csail.mit.edu>
2011-07-21 20:32:25 -0400

    basic query rewriting machinery works for the one test query.
    input query: SELECT x.a, y.b + 2, y.c, y.cc AS ycc FROM x, y as yy1, y as yy2 WHERE x.bb = yy1.b AND yy1.k1 = yy2.k2 AND (yy2.d > 7 OR yy2.e = (3+4)) AND (yy1.f='hello') AND yy2.cc = 9
    
    output query: select `anontab_x`.`anonfld_a` AS `a`,(`anontab_y`.`anonfld_b` + 1002) AS `y.b + 2`,`anontab_y`.`anonfld_c` AS `c`,`anontab_y`.`anonfld_cc` AS `ycc` from ``.`anontab_x` join ``.`anontab_y` `anontab_yy1` join ``.`anontab_y` `anontab_yy2` where ((`anontab_x`.`anonfld_bb` = `anontab_yy1`.`anonfld_b`) and (`anontab_yy1`.`anonfld_k1` = `anontab_yy2`.`anonfld_k2`) and ((`anontab_yy2`.`anonfld_d` > 1007) or (`anontab_yy2`.`anonfld_e` = (1003 + 1004))) and (`anontab_yy1`.`anonfld_f` = 'ENCRYPTED:hello') and (`anontab_yy2`.`anonfld_cc` = 1009))

--- src/parser/xftest.cc
+++ src/parser/xftest.cc
@@ -42,6 +42,28 @@ class CryptItem {
             Item_func *ifn = (Item_func *) i;
 
             switch (ifn->functype()) {
+            case Item_func::Functype::EQ_FUNC:
+            case Item_func::Functype::NE_FUNC:
+            case Item_func::Functype::GT_FUNC:
+            case Item_func::Functype::GE_FUNC:
+            case Item_func::Functype::LT_FUNC:
+            case Item_func::Functype::LE_FUNC:
+            {
+                Item **args = ifn->arguments();
+
+                CryptItem a(args[0]);
+                CryptItem b(args[1]);
+                Item *n;
+                if (ifn->functype() == Item_func::Functype::EQ_FUNC)
+                    n = new Item_func_eq(a.rewrite(), b.rewrite());
+                else if (ifn->functype() == Item_func::Functype::GT_FUNC)
+                    n = new Item_func_gt(a.rewrite(), b.rewrite());
+                else
+                    fatal() << "bug";
+                n->name = i->name;
+                return n;
+            }
+
             case Item_func::Functype::UNKNOWN_FUNC:
             {
                 std::string name = ifn->func_name();
@@ -55,20 +77,69 @@ class CryptItem {
                     n->name = i->name;
                     return n;
                 } else {
-                    std::cerr << "not rewriting function " << name << std::endl;
-                    return i;
+                    fatal() << "unknown function " << name;
                 }
             }
 
             default:
-                std::cerr << "not rewriting function " << ifn->functype() << std::endl;
-                return i;
+                fatal() << "unknown functype " << ifn->functype();
             }
         }
 
+        case Item::Type::COND_ITEM: {
+            Item_cond *ic = (Item_cond *) i;
+            switch (ic->functype()) {
+            case Item_func::Functype::COND_AND_FUNC:
+            case Item_func::Functype::COND_OR_FUNC:
+            {
+                List<Item> *arglist = ic->argument_list();
+                List<Item> newlist;
+
+                auto it = List_iterator<Item>(*arglist);
+                for (;;) {
+                    Item *argitem = it++;
+                    if (!argitem)
+                        break;
+
+                    CryptItem ci(argitem);
+                    newlist.push_back(ci.rewrite());
+                }
+
+                Item *n;
+                if (ic->functype() == Item_func::Functype::COND_AND_FUNC)
+                    n = new Item_cond_and(newlist);
+                else if (ic->functype() == Item_func::Functype::COND_OR_FUNC)
+                    n = new Item_cond_or(newlist);
+                else
+                    fatal() << "bug";
+                n->name = i->name;
+                return n;
+            }
+
+            default:
+                fatal() << "unknown cond functype " << ic->functype();
+            }
+        }
+
+        case Item::Type::STRING_ITEM: {
+            Item_string *is = (Item_string *) i;
+            std::string s("ENCRYPTED:");
+            s += std::string(is->str_value.ptr(), is->str_value.length());
+            Item *n = new Item_string(strdup(s.c_str()), s.size(),
+                                      is->str_value.charset());
+            n->name = i->name;
+            return n;
+        }
+
+        case Item::Type::INT_ITEM: {
+            Item_num *in = (Item_num *) i;
+            Item *n = new Item_int(in->val_int() + 1000);
+            n->name = i->name;
+            return n;
+        }
+
         default:
-            std::cerr << "not rewriting type " << i->type() << std::endl;
-            return i;
+            fatal() << "unknown type " << i->type();
         }
     }
 
@@ -89,31 +160,6 @@ recurse(THD *t, Item *i)
 
     switch (i->type()) {
     // see types in mysql-server/sql/item.h: enum Type
-    case Item::Type::COND_ITEM: {
-        Item_cond *ic = (Item_cond *) i;
-        switch (ic->functype()) {
-        case Item_func::Functype::COND_AND_FUNC:
-        case Item_func::Functype::COND_OR_FUNC:
-        {
-            List<Item> *arglist = ic->argument_list();
-
-            int xr = 0;
-            auto it = List_iterator<Item>(*arglist);
-            for (;; ) {
-                Item *argitem = it++;
-                if (!argitem)
-                    break;
-
-                int xi = recurse(t, argitem);
-                xr += xi;
-            }
-            return xr;
-        }
-
-        default:
-            fatal() << "unknown cond functype " << ic->functype();
-        }
-    }
 
     case Item::Type::FUNC_ITEM: {
         Item_func *ifn = (Item_func *) i;
@@ -154,18 +200,6 @@ recurse(THD *t, Item *i)
         }
     }
 
-    case Item::Type::STRING_ITEM: {
-        Item_string *is = (Item_string *) i;
-        printf("recurse: string item %s\n", is->str_value.c_ptr());
-        return 0;
-    }
-
-    case Item::Type::INT_ITEM: {
-        Item_num *in = (Item_num *) i;
-        printf("recurse: int item %lld\n", in->val_int());
-        return in->val_int();
-    }
-
     case Item::Type::FIELD_ITEM: {
         Item_field *ifl = (Item_field *) i;
         printf("recurse: field item %s.%s.%s\n",
@@ -191,7 +225,11 @@ xftest(void)
         printf("init_errmessage error\n");
 
     const char *q =
-        "SELECT x.a, y.b + 2, y.c, y.cc AS ycc FROM x, y as yy1, y as yy2 WHERE x.bb = yy1.b AND yy1.k1 = yy2.k2 AND (yy2.d > 7 OR yy2.e = (3+4)) AND (yy1.f='hello') AND yy2.cc = 9";
+        "SELECT x.a, y.b + 2, y.c, y.cc AS ycc "
+        "FROM x, y as yy1, y as yy2 "
+        "WHERE x.bb = yy1.b AND yy1.k1 = yy2.k2 AND "
+              "(yy2.d > 7 OR yy2.e = (3+4)) AND (yy1.f='hello') AND "
+              "yy2.cc = 9";
     char buf[1024];
     strlcpy(buf, q, sizeof(buf));
     size_t len = strlen(buf);
@@ -208,7 +246,7 @@ xftest(void)
 
         t->set_db("", 0);
 
-        printf("q=%s\n", buf);
+        printf("input query: %s\n", buf);
         bool error = parse_sql(t, &ps, 0);
         if (error) {
             printf("parse error: %d %d %d\n", error, t->is_fatal_error,
@@ -216,7 +254,7 @@ xftest(void)
             printf("parse error: h %p\n", t->get_internal_handler());
             printf("parse error: %d %s\n", t->is_error(), t->stmt_da->message());
         } else {
-            printf("command %d\n", lex.sql_command);
+            //printf("command %d\n", lex.sql_command);
 
             // iterate over the entire select statement..
             // based on st_select_lex::print in mysql-server/sql/sql_select.cc
@@ -229,24 +267,41 @@ xftest(void)
                 if (!item)
                     break;
 
-                int x = recurse(t, item);
-
                 CryptItem ci(item);
                 String s;
                 Item *newitem = ci.rewrite();
                 newitem->print(&s, QT_ORDINARY);
-                printf("rewrite: %s (x=%d, alias=%s)\n", s.c_ptr(), x, item->name);
                 new_item_list.push_back(newitem);
             }
             lex.select_lex.item_list = new_item_list;
 
-            //int x = recurse(t, lex.select_lex.where);
-            //printf("x=%d\n", x);
+            auto join_it = List_iterator<TABLE_LIST>(lex.select_lex.top_join_list);
+            List<TABLE_LIST> new_join_list;
+            for (;;) {
+                TABLE_LIST *t = join_it++;
+                if (!t)
+                    break;
+
+                TABLE_LIST *nt = new TABLE_LIST();
+                std::string db(t->db, t->db_length);
+                std::string table_name(t->table_name, t->table_name_length);
+                std::string alias(t->alias);
+                table_name = "anontab_" + table_name;
+                alias = "anontab_" + alias;
+                nt->init_one_table(strdup(db.c_str()), db.size(),
+                                   strdup(table_name.c_str()), table_name.size(),
+                                   strdup(alias.c_str()), t->lock_type);
+                new_join_list.push_back(nt);
+            }
+            lex.select_lex.top_join_list = new_join_list;
+
+            CryptItem wi(lex.select_lex.where);
+            lex.select_lex.where = wi.rewrite();
 
             String s;
             lex.select_lex.print(t, &s, QT_ORDINARY);
             //lex.unit.print(&s, QT_ORDINARY);
-            printf("reconstructed query: %s\n", s.c_ptr());
+            printf("output query: %s\n", s.c_ptr());
         }
 
         t->end_statement();
