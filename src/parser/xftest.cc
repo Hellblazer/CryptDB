4c5b75f705cc0a8f9f7df62efdd830dea28b9e14
Nickolai Zeldovich <nickolai@csail.mit.edu>
2011-07-25 15:07:48 -0400

    dispatch logic for query rewriting

--- src/parser/xftest.cc
+++ src/parser/xftest.cc
@@ -1,4 +1,9 @@
 #include <string>
+#include <map>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
 #include <stdio.h>
 #include <bsd/string.h>
 
@@ -18,144 +23,206 @@
 #include "derror.h"
 #include "item.h"
 
-class CryptItem {
+#define CONCAT2(a, b)   a ## b
+#define CONCAT(a, b)    CONCAT2(a, b)
+#define ANON            CONCAT(__anon_id_, __LINE__)
+
+class CItemType {
  public:
-    CryptItem(Item *iarg) : i(iarg)
-    {
+    virtual CItemType *getSpecific(Item *) { return this; }
+    virtual Item *do_rewrite(Item *) = 0;
+};
+
+template<class T>
+class CItemSubtype : public CItemType {
+ public:
+    virtual Item *do_rewrite(Item *i) { return do_rewrite((T*) i); }
+    virtual Item *do_rewrite(T *) = 0;
+};
+
+/*
+ * Directories for locating an appropriate CItemType for a given Item.
+ */
+template <class T>
+class CItemTypeDir : public CItemType {
+ public:
+    void reg(T t, CItemType *ct) { types[t] = ct; }
+
+    CItemType *lookup(Item *i, T t, const char *errname) {
+        auto x = types.find(t);
+        if (x == types.end()) {
+            stringstream ss;
+            ss << "unhandled " << errname << " " << t;
+            throw std::runtime_error(ss.str());
+        }
+        return x->second->getSpecific(i);
     }
 
-    Item *
-    rewrite(void)
-    {
-        switch (i->type()) {
-        case Item::Type::FIELD_ITEM:
-        {
-            Item_field *ifl = (Item_field *) i;
-            Item *n =
-                new Item_field(0,
-                               ifl->db_name,
-                               strdup((std::string("anontab_") +
-                                       ifl->table_name).c_str()),
-                               strdup((std::string("anonfld_") +
-                                       ifl->field_name).c_str()));
-            n->name = i->name;
-            return n;
-        }
-
-        case Item::Type::FUNC_ITEM: {
-            Item_func *ifn = (Item_func *) i;
-
-            switch (ifn->functype()) {
-            case Item_func::Functype::EQ_FUNC:
-            case Item_func::Functype::NE_FUNC:
-            case Item_func::Functype::GT_FUNC:
-            case Item_func::Functype::GE_FUNC:
-            case Item_func::Functype::LT_FUNC:
-            case Item_func::Functype::LE_FUNC:
-            {
-                Item **args = ifn->arguments();
-
-                CryptItem a(args[0]);
-                CryptItem b(args[1]);
-                Item *n;
-                if (ifn->functype() == Item_func::Functype::EQ_FUNC)
-                    n = new Item_func_eq(a.rewrite(), b.rewrite());
-                else if (ifn->functype() == Item_func::Functype::GT_FUNC)
-                    n = new Item_func_gt(a.rewrite(), b.rewrite());
-                else
-                    fatal() << "bug";
-                n->name = i->name;
-                return n;
-            }
-
-            case Item_func::Functype::UNKNOWN_FUNC:
-            {
-                std::string name = ifn->func_name();
-                Item **args = ifn->arguments();
-
-                if (name == "+") {
-                    CryptItem a(args[0]);
-                    CryptItem b(args[1]);
-                    Item *n = new Item_func_plus(a.rewrite(),
-                                                 b.rewrite());
-                    n->name = i->name;
-                    return n;
-                } else {
-                    fatal() << "unknown function " << name;
-                }
-            }
-
-            default:
-                fatal() << "unknown functype " << ifn->functype();
-            }
-        }
-
-        case Item::Type::COND_ITEM: {
-            Item_cond *ic = (Item_cond *) i;
-            switch (ic->functype()) {
-            case Item_func::Functype::COND_AND_FUNC:
-            case Item_func::Functype::COND_OR_FUNC:
-            {
-                List<Item> *arglist = ic->argument_list();
-                List<Item> newlist;
-
-                auto it = List_iterator<Item>(*arglist);
-                for (;; ) {
-                    Item *argitem = it++;
-                    if (!argitem)
-                        break;
-
-                    CryptItem ci(argitem);
-                    newlist.push_back(ci.rewrite());
-                }
-
-                Item *n;
-                if (ic->functype() == Item_func::Functype::COND_AND_FUNC)
-                    n = new Item_cond_and(newlist);
-                else if (ic->functype() == Item_func::Functype::COND_OR_FUNC)
-                    n = new Item_cond_or(newlist);
-                else
-                    fatal() << "bug";
-                n->name = i->name;
-                return n;
-            }
-
-            default:
-                fatal() << "unknown cond functype " << ic->functype();
-            }
-        }
-
-        case Item::Type::STRING_ITEM: {
-            Item_string *is = (Item_string *) i;
-            std::string s("ENCRYPTED:");
-            s += std::string(is->str_value.ptr(), is->str_value.length());
-            Item *n = new Item_string(strdup(s.c_str()), s.size(),
-                                      is->str_value.charset());
-            n->name = i->name;
-            return n;
-        }
-
-        case Item::Type::INT_ITEM: {
-            Item_num *in = (Item_num *) i;
-            Item *n = new Item_int(in->val_int() + 1000);
-            n->name = i->name;
-            return n;
-        }
-
-        case Item::Type::SUBSELECT_ITEM: {
-            // XXX handle sub-selects
-            return i;
-        }
-
-        default:
-            fatal() << "unknown type " << i->type();
-        }
+    Item *do_rewrite(Item *) {
+        throw std::runtime_error("directory cannot rewrite");
     }
 
  private:
-    Item *i;
+    std::map<T, CItemType*> types;
 };
 
+static class CItemBaseDir : public CItemTypeDir<Item::Type> {
+ public:
+    CItemType *getSpecific(Item *i) {
+        return lookup(i, i->type(), "type");
+    }
+} itemTypes;
+
+static class CItemFuncDir : public CItemTypeDir<Item_func::Functype> {
+ public:
+    CItemFuncDir() {
+        itemTypes.reg(Item::Type::FUNC_ITEM, this);
+        itemTypes.reg(Item::Type::COND_ITEM, this);
+    }
+    CItemType *getSpecific(Item *i) {
+        return lookup(i, ((Item_func *) i)->functype(), "func type");
+    }
+} funcTypes;
+
+static class CItemFuncNameDir : public CItemTypeDir<std::string> {
+ public:
+    CItemFuncNameDir() { funcTypes.reg(Item_func::Functype::UNKNOWN_FUNC, this); }
+    CItemType *getSpecific(Item *i) {
+        return lookup(i, ((Item_func *) i)->func_name(), "func name");
+    }
+} funcNames;
+
+/*
+ * Helper functions to look up via directory & invoke method.
+ */
+static Item *
+rewrite(Item *i)
+{
+    CItemType *t = itemTypes.getSpecific(i);
+    Item *n = t->do_rewrite(i);
+    n->name = i->name;
+    return n;
+}
+
+/*
+ * CItemType classes for supported Items.
+ */
+static class CItemField : public CItemSubtype<Item_field> {
+ public:
+    CItemField() {
+        itemTypes.reg(Item::Type::FIELD_ITEM, this);
+    }
+
+    Item *do_rewrite(Item_field *i) {
+        return
+            new Item_field(0,
+                           i->db_name,
+                           strdup((std::string("anontab_") +
+                                   i->table_name).c_str()),
+                           strdup((std::string("anonfld_") +
+                                   i->field_name).c_str()));
+    }
+} ANON;
+
+static class CItemString : public CItemSubtype<Item_string> {
+ public:
+    CItemString() {
+        itemTypes.reg(Item::Type::STRING_ITEM, this);
+    }
+
+    Item *do_rewrite(Item_string *i) {
+        std::string s("ENCRYPTED:");
+        s += std::string(i->str_value.ptr(), i->str_value.length());
+        return new Item_string(strdup(s.c_str()), s.size(),
+                               i->str_value.charset());
+    }
+} ANON;
+
+static class CItemInt : public CItemSubtype<Item_num> {
+ public:
+    CItemInt() {
+        itemTypes.reg(Item::Type::INT_ITEM, this);
+    }
+
+    Item *do_rewrite(Item_num *i) {
+        return new Item_int(i->val_int() + 1000);
+    }
+} ANON;
+
+static class CItemSubselect : public CItemType {
+ public:
+    CItemSubselect() {
+        itemTypes.reg(Item::Type::SUBSELECT_ITEM, this);
+    }
+
+    Item *do_rewrite(Item *i) {
+        // XXX handle sub-selects
+        return i;
+    }
+} ANON;
+
+template<Item_func::Functype FT, class IT>
+class CItemCompare : public CItemSubtype<Item_func> {
+ public:
+    CItemCompare() {
+        funcTypes.reg(FT, this);
+    }
+
+    Item *do_rewrite(Item_func *i) {
+        Item **args = i->arguments();
+        return new IT(rewrite(args[0]), rewrite(args[1]));
+    }
+};
+
+static CItemCompare<Item_func::Functype::EQ_FUNC, Item_func_eq> ANON;
+static CItemCompare<Item_func::Functype::NE_FUNC, Item_func_ne> ANON;
+static CItemCompare<Item_func::Functype::GT_FUNC, Item_func_gt> ANON;
+static CItemCompare<Item_func::Functype::GE_FUNC, Item_func_ge> ANON;
+static CItemCompare<Item_func::Functype::LT_FUNC, Item_func_lt> ANON;
+static CItemCompare<Item_func::Functype::LE_FUNC, Item_func_le> ANON;
+
+template<Item_func::Functype FT, class IT>
+class CItemCond : public CItemSubtype<Item_cond> {
+ public:
+    CItemCond() {
+        funcTypes.reg(FT, this);
+    }
+
+    Item *do_rewrite(Item_cond *i) {
+        List<Item> *arglist = i->argument_list();
+        List<Item> newlist;
+
+        auto it = List_iterator<Item>(*arglist);
+        for (;; ) {
+            Item *argitem = it++;
+            if (!argitem)
+                break;
+
+            newlist.push_back(rewrite(argitem));
+        }
+
+        return new IT(newlist);
+    }
+};
+
+static CItemCond<Item_func::Functype::COND_AND_FUNC, Item_cond_and> ANON;
+static CItemCond<Item_func::Functype::COND_OR_FUNC,  Item_cond_or>  ANON;
+
+static class CItemPlus : public CItemSubtype<Item_func> {
+ public:
+    CItemPlus() {
+        funcNames.reg("+", this);
+    }
+
+    Item *do_rewrite(Item_func *i) {
+        Item **args = i->arguments();
+        return new Item_func_plus(rewrite(args[0]), rewrite(args[1]));
+    }
+} ANON;
+
+/*
+ * Test harness.
+ */
 static void
 xftest(void)
 {
@@ -206,8 +273,7 @@ xftest(void)
                 if (!item)
                     break;
 
-                CryptItem ci(item);
-                Item *newitem = ci.rewrite();
+                Item *newitem = rewrite(item);
                 new_item_list.push_back(newitem);
             }
             lex.select_lex.item_list = new_item_list;
@@ -235,9 +301,7 @@ xftest(void)
                 new_join_list.push_back(nt);
             }
             lex.select_lex.top_join_list = new_join_list;
-
-            CryptItem wi(lex.select_lex.where);
-            lex.select_lex.where = wi.rewrite();
+            lex.select_lex.where = rewrite(lex.select_lex.where);
 
             cout << "output query: " << lex << endl;
         }
