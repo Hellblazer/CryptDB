0f8e22a446a2a951a10fdc0256bc4bf7a13bab25
Catherine Redfield <cat_red@mit.edu>
2011-08-24 19:10:19 -0700

    fixed createstate to also initialize some number of posts

--- src/eval/createstate.py
+++ src/eval/createstate.py
@@ -49,17 +49,39 @@ pm_send_update = "UPDATE phpbb_users SET user_lastpost_time = 1314137570 WHERE u
 
 
 
+post_header = "INSERT INTO phpbb_posts  (forum_id, poster_id, icon_id, poster_ip, post_time, post_approved, enable_bbcode, enable_smilies, enable_magic_url, enable_sig, post_username, post_subject, post_text, post_checksum, post_attachment, bbcode_bitfield, bbcode_uid, post_postcount, post_edit_locked, topic_id) VALUES (2, "
+#user_id
+post_footer = ", 0, '172.16.224.142', 1314236475, 1, 1, 1, 1, 1, '', 'Re: Welcome to phpBB3', 'posting!! posting!! posting!! posting!! posting!!', '8729fffdaee7fbf4345410caa27628dc', 0, '', 'emb5qn35', 1, 0, 1);\n"
+
+post_config = "UPDATE phpbb_config SET config_value = config_value + 1 WHERE config_name = 'num_posts';\n"
+
+post_topic_header = "UPDATE phpbb_topics SET topic_last_view_time = 1314236475, topic_replies_real = topic_replies_real + 1, topic_bumped = 0, topic_bumper = 0, topic_replies = topic_replies + 1, topic_last_post_id = topic_last_post_id + 1, topic_last_poster_id = "
+#author_id
+post_topic_middle = ", topic_last_poster_name = '"
+#user+author_id
+post_topic_footer = "', topic_last_poster_colour = 'AA0000', topic_last_post_subject = 'Re: Welcome to phpBB3', topic_last_post_time = 1314236475 WHERE topic_id = 1;\n"
+
+post_user = "UPDATE phpbb_users SET user_lastpost_time = 1314236475, user_posts = user_posts + 1 WHERE user_id = "
+#author_id
+
+post_forum_header = "UPDATE phpbb_forums SET forum_posts = forum_posts + 1, forum_last_post_id = forum_last_post_id + 1, forum_last_post_subject = 'Re: Welcome to phpBB3', forum_last_post_time = 1314236475, forum_last_poster_id = "
+#author_id
+post_forum_middle = ", forum_last_poster_name = '"
+#user+author_id
+post_forum_footer = "', forum_last_poster_colour = 'AA0000' WHERE forum_id = 2;\n"
+
 
 def main(arg):
     
-    if len(arg) != 5:
-        print 'wrong nr of args: python createstate.py no-users no-messages headerfile outputfile'
+    if len(arg) != 6:
+        print 'wrong nr of args: python createstate.py no-users no-messages no-posts headerfile outputfile'
         return
     
     users = int(arg[1])
     messages = int(arg[2])
-    filename = arg[4]
-    headerfile = arg[3]
+    posts = int(arg[3])
+    headerfile = arg[4]
+    filename = arg[5]
     
     f = open(headerfile, 'w')
     f.write(header)
@@ -113,6 +135,23 @@ def main(arg):
 
         query = pm_send_update
         f.write(query)
+
+    for i in range(0, posts):
+        user_id = str((i % users) + uidstart)
+
+        query = post_header + user_id + post_footer
+        f.write(query)
+
+        f.write(post_config)
+
+        query = post_topic_header + user_id + post_topic_middle + "user" + user_id + post_topic_footer
+        f.write(query)
+
+        query = post_user +  user_id + ";\n"
+        f.write(query)
+        
+        query = post_forum_header + user_id + post_forum_middle + "user" + user_id + post_forum_footer
+        f.write
 
     f.close()
 
